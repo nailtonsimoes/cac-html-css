9dc10a844e8bdc84846655244a953cc193d06d49
nailtonsimoes <nailtonsimoes@gmail.com>
2021-11-24 12:39:30 -0300

    projeto 70% completo

---
+++ main.js
@@ -0,0 +1,12 @@
+var circle = document.getElementById('circle')
+var upBtn = document.getElementById('upBtn')
+var downBtn = document.getElementById('downBtn')
+
+var rotateValue = circle.style.transform
+var rotateSum
+
+upBtn.oneclick = function () {
+  rotateSum = rotateValue + 'rotate(-90deg)'
+  circle.style.transform = rotateSum
+  rotateValue = rotateSum
+}
